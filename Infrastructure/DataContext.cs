ed0abfa965393952ab89e32d67cf43cfe7e1357a
abdulrahman makktof <alraies.com@gmail.com>
2021-01-25 16:00:40 +0300

    dbContext work

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) 
+            :base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<PortfoiloItem>().Property(x => x.Id).HasDefaultValue("NEWID()");
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id=Guid.NewGuid(),
+                    Avatar="avatar.jpg",
+                    FullName="Alraies Makktof",
+                    Profile="Backend Web Programmer"
+                }
+                );
+        }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<PortfoiloItem> portfoiloItems { get; set; }
+       
+        
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,6 +1,11 @@
+
+
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +17,21 @@ namespace Web
 {
     public class Startup
     {
+        private readonly IConfiguration configration;
+
+        public Startup(IConfiguration configration)
+        {
+            this.configration = configration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(configration.GetConnectionString("MyPortfolioDb"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
